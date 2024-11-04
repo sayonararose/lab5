1a5e8c4e8b86fc95d62e2d77b38f990dcc028482
sayonararose <159807074+sayonararose@users.noreply.github.com>
2024-11-04 19:19:10 +0200

    changes

---
+++ task.js
@@ -0,0 +1,24 @@
+//Функція для отримання даних від користувача
+function userInput(){
+    let country = prompt('Введіть назву країни доставки');
+    let price = getValidNumber('Введіть загальну вартість товару');
+    let deliveryFee = getValidNumber('Введіть вартість доставки товару');
+
+    const message = getShippingMessage(country, price, deliveryFee)
+    console.log(message);
+}
+// Функція для перевірки введеного числа
+function getValidNumber(promptMessage) {
+    let value;
+    while (true) {
+        value = Number(prompt(promptMessage));
+        if (!isNaN(value) && value > 0 && value !== null && value) {
+            return value;
+        }
+        console.log("Будь ласка, введіть правильне число (додатнє значення).");
+    }
+}
+//Функція для формування повідомлення про доставку (Куди і загальна вартість(вартість товару,вартість доставки))
+function getShippingMessage(country, price, deliveryFee){
+    return `Доставка до ${country} загальна вартість ${price + deliveryFee} кредитів`;
+}
